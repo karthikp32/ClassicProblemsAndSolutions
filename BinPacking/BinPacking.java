a1545219ce216c42e0d6b9c756f6f0c6b93e1ae5
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-06-26 22:14:54 +0000

    fixed bug for sorting int[] in descending order, got first-fit decreasing heuristic variant working

--- BinPacking/BinPacking.java
+++ BinPacking/BinPacking.java
@@ -17,8 +17,7 @@ public class BinPacking {
     //time: O(nlogn)
     //space: O(n)
     public static int getMinimumWorkSessions(int[] tasks, int sessionTime) {
-        int[] sortedTasks = Arrays.copyOf(tasks, tasks.length);
-        Arrays.sort(sortedTasks);
+        int[] sortedTasks = getArrayInDescendingOrder(tasks);
         int currTaskHours = 0, sessions = 0;
         for (int i=0; i < sortedTasks.length; i++) {
             if (currTaskHours + sortedTasks[i] <= sessionTime) {
@@ -28,7 +27,22 @@ public class BinPacking {
                 currTaskHours = sortedTasks[i];
             }
         }
-        return currTaskHours > 0 ? sessions++ : sessions;
+        return currTaskHours > 0 ? sessions + 1 : sessions;
+    }
+
+    public static int[] getArrayInDescendingOrder(int[] tasks) {
+      int[] sortedTasks = Arrays.copyOf(tasks, tasks.length);
+      Arrays.sort(sortedTasks);
+      int i=0, j=sortedTasks.length - 1, temp=0;
+      while (i <= j) {
+        temp = sortedTasks[i];
+        sortedTasks[i] = sortedTasks[j];
+        sortedTasks[j] = temp;
+        i++;
+        j--;
+      }
+      
+      return sortedTasks;
     }
 
     public static void main(String args[]) {
@@ -45,19 +59,23 @@ public class BinPacking {
       int sessionTime2 = 8;
       int expectedMinWorkSessions2 = 2;
       int actualMinWorkSessions2 = getMinimumWorkSessions(tasks2, sessionTime2);
-
+      System.out.println("Expected minimum work sessions are " + expectedMinWorkSessions2 
+      + " while actual minimum work sessions are " + actualMinWorkSessions2);
       
       int[] tasks3 = {10,10,7,8,1};
       int sessionTime3 = 15;
       int expectedMinWorkSessions3 = 3;
       int actualMinWorkSessions3 = getMinimumWorkSessions(tasks3, sessionTime3);
-
+      System.out.println("Expected minimum work sessions are " + expectedMinWorkSessions3 
+      + " while actual minimum work sessions are " + actualMinWorkSessions3 + ". Since we used a heuristic, we got a less than "
+      + "optimal but good enough solution. ");
 
       int[] tasks4 = {1,2,3,4,5};
       int sessionTime4 = 15;
       int expectedMinWorkSessions4 = 1;
       int actualMinWorkSessions4 = getMinimumWorkSessions(tasks4, sessionTime4);
-
+      System.out.println("Expected minimum work sessions are " + expectedMinWorkSessions4 
+      + " while actual minimum work sessions are " + actualMinWorkSessions4);
 
     }
 }
