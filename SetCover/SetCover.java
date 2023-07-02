b5ed605b6ebd2048a3462767f2bd5fd5bd385f11
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-07-02 21:00:48 +0000

    add comments for implementing effective and efficient heuristic.

---
+++ SetCover/SetCover.java
@@ -0,0 +1,26 @@
+package SetCover;
+
+//What is the set cover problem?
+//Given a collection of sets S={S1, ..., Sm} and a universal set U = {1,...,n} that contains all of the elements
+//find the minimum number of sets that will include all of the elements 
+//in the universal set
+
+//Used https://leetcode.com/problems/smallest-sufficient-team/description/
+//as a starting point
+//This is an instance of the set cover problem since given a list of required skills ( universal set U = {1,...,n})
+//and people with some of those skills (set of some or all of those skills S={S1, ..., Sm}) , you want to figure out the 
+//smallest number of people you have to hire to have all of skills.
+
+public class SetCover {
+    
+    //Formulation of this set cover problem
+    //as integer linear programming problem
+
+
+    //Implementation of an effective and efficient heuristic to 
+    //this linear programming problem
+
+    public static void main(String[] args) {
+
+    }
+}
