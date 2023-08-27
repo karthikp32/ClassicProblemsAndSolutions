0107f3d46a083e3afbd02a9f9ed94795a07ee31d
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2023-08-27 18:07:38 +0000

    created skeleton code for TravelingSalesman class

---
+++ TravelingSalesman/TravelingSalesman.java
@@ -0,0 +1,40 @@
+package TravelingSalesman;
+
+public class TravelingSalesman {
+    
+    //Traveling Salesman Problem: 
+    //Input: A weighted graph where the edges have weights like $, distance, time spent, energy spent, etc.
+    //Output/problem description: Get the cycle with the minimum cost that visits each of the nodes in the graph
+    //only once
+    //Example: 
+    //Input:  1  2   3
+    //Output: 1---2---3
+
+
+    public class Graph {
+
+        //Common representations of graph:
+        //1) Matrix
+        //2) Adjacency List
+        //3) Adjacency matrix
+        //4) Edge matrix
+
+        public Graph() {
+
+        }
+    }
+
+
+    public void createGraph() {
+
+    }
+
+    //Implement Kernighan-Lin/K-optimal tours heuristic
+    //where K = 2 so 2-optimal tours heuristic
+    public Graph getMinimumCostCycle() {
+        return new Graph();
+    }
+    public static void main(String[] args) {
+
+    }
+}
